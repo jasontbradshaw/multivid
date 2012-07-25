a7a316c6a953b536a459c6cb638a2b0cee11b61d
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2012-07-24 23:24:17 -0500

    Implement basic autocomplete

--- static/js/main.js
+++ static/js/main.js
@@ -1,61 +1,156 @@
 require.config({
     paths: {
-        jquery: "/static/js/libs/jquery-min",
-        underscore: "/static/js/libs/underscore-min",
-        backbone: "/static/js/libs/backbone-min",
-        mustache: "/static/js/libs/mustache",
+        jquery: 'libs/jquery-min',
+        underscore: 'libs/underscore-min',
+        backbone: 'libs/backbone-min',
+        mustache: 'libs/mustache',
     },
     shim: {
         jquery: {
-            exports: "$"
+            exports: '$'
         },
         underscore: {
-            exports: "_"
+            exports: '_'
         },
         backbone: {
-            deps: ["underscore", "jquery"],
-            exports: "Backbone"
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
         }
     },
 
     // add dummy params to break caching
-    urlArgs: "__nocache__=" +  (new Date()).getTime()
+    urlArgs: '__nocache__=' +  (new Date()).getTime()
 });
 
-require(["jquery", "underscore","backbone", "mustache"],
-function ($, _, Backbone, Mustache) {
-$(function () {
+require([
+    'jquery',
+    'underscore',
+    'backbone',
+    'mustache',
+    'text!/static/templates/search_bar.html.mustache',
+    'text!/static/templates/ac_suggestion.html.mustache'
+],
+function ($, _, Backbone, Mustache, tmplSearchBar, tmplAcSuggestion) {
 
 // the search bar
 var SearchBar = Backbone.Model.extend({
-    initialize: function () {
-        this.bind("change:position", function () {
-            // TODO: change CSS class
-            console.log("position changed!");
-        });
-    },
     defaults: {
-        position: "center" // the current location of the search bar/box
+        query: '',
+
+        // minimum amount of time b/w AJAX calls
+        minUpdateIntervalMs: 200,
+        updateTimeout: null // id for the last update timeout
+    },
+
+    initialize: function () {
+        this.view = new SearchBarView({model: this});
+        this.acSuggestionList = new AutocompleteSuggestionList();
+    },
+
+    updateQuery: function (query) {
+        if (query !== this.get('query')) {
+            this.set({'query': query});
+
+            // clear any existing update timeout
+            if (this.get('updateTimeout') !== null) {
+                clearTimeout(this.get('updateTimeout'));
+            }
+
+            var self = this;
+            var timeoutId = setTimeout(function () {
+                self.acSuggestionList.update(query);
+
+                // clear the timeout id once done
+                self.set({'updateTimeout': null});
+
+            }, this.get('minUpdateIntervalMs'));
+
+            // store the timeout id for the next update call
+            this.set({'updateTimeout': timeoutId});
+        }
     }
 });
 
 var SearchBarView = Backbone.View.extend({
-    id: "search-bar",
-    className: "search-bar-position-center"
-});
+    template: Mustache.compile(tmplSearchBar),
 
-// a suggestion below the search bar
-var AutocompleteSuggestion = Backbone.Model.extend({
-    defaults: {
-        originator: null,
-        text: ""
+    events: {
+        'keyup input': 'updateQuery',
+        'keydown input': 'updateQuery'
+    },
+
+    initialize: function () {
+        // create the element and add it to the document
+        this.setElement($(this.template({placeholder: 'search'})));
+        this.$el.appendTo($('body'));
+
+        // cache a ref to the input and focus it
+        this.$input = this.$el.find('input');
+        this.$input.focus();
+    },
+
+    updateQuery: function (e) {
+        this.model.updateQuery(this.$input.val());
     }
 });
 
-var AutocompleteSuggestionCollection = Backbone.Collection.extend({
-    model: AutocompleteSuggestion
+// a single suggestion below the search bar
+var AutocompleteSuggestion = Backbone.Model.extend({
+    defaults: {
+        provider: null,
+        suggestion: ''
+    }
 });
 
-// ** END **
+// the suggestions collection below the search bar
+var AutocompleteSuggestionList = Backbone.Collection.extend({
+    model: AutocompleteSuggestion,
+    url: '/search/autocomplete',
+
+    initialize: function () {
+        this.view = new AutocompleteSuggestionListView({
+            el: $('#search-bar ul'),
+            collection: this
+        });
+    },
+
+    update: function (query) {
+        var xhr = $.getJSON(this.url, {'query': query});
+
+        // update the collection on reset
+        var collection = this;
+        xhr.success(function (data) {
+            collection.reset(data.results);
+        });
+    }
 });
+
+var AutocompleteSuggestionListView = Backbone.View.extend({
+    template: Mustache.compile(tmplAcSuggestion),
+
+    initialize: function () {
+        this.collection.on('reset', this.render, this);
+    },
+
+    render: function () {
+        // get the suggestion list from the collection
+        var suggestions = this.collection.toJSON();
+
+        // clear out the old suggestions
+        this.$el.children().remove();
+
+        // add all the new suggestions
+        _.each(suggestions, function (suggestion) {
+            this.$el.append(this.template(suggestion));
+        }, this);
+
+        return this;
+    },
+});
+
+// ENTRY POINT
+$(function () {
+    var searchBar = new SearchBar();
+});
+
 });
